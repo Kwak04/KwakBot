d6c081887a0a85d73a484c26cb17e849087b26bb
Kwak04 <tronixdevelop@gmail.com>
2019-10-22 18:59:39 +0900

    fix: changed code about sending message to new API

--- TestBot.py
+++ TestBot.py
@@ -2,29 +2,21 @@
 # client secret : iXr8dowBXR2w_76XaBMGbnbGwg8Axb5H
 # token : NTkwOTM2NzMzNzg1NTIyMjA1.XQpfOA.2655fVoRtNpa52RcIn-AoGqGOcA
 
+
 import discord
-import asyncio
-
-client = discord.Client()
-token = "NTkwOTM2NzMzNzg1NTIyMjA1.XQpfOA.2655fVoRtNpa52RcIn-AoGqGOcA"
 
 
-@client.event
-async def on_ready():
-    game = discord.Game("Just testing")
-    await client.change_presence(activity=game)
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print('Logged on as {0}!'.format(self.user))
+
+    async def on_message(self, message):
+        print('Message from {0.author}: {0.content}'.format(message))
+        channel = message.channel
+
+        if message.content == "ㅎㅇ":
+            await channel.send('안녕!')
 
 
-@client.event
-async def on_message(message):
-
-    channel = message.channel
-
-    if message.author.bot:
-        return None
-
-    if message.content == "테스트":
-        await channel.send("테스트입니다!")
-
-
-client.run(token)
+client = MyClient()
+client.run("NTkwOTM2NzMzNzg1NTIyMjA1.XQpfOA.2655fVoRtNpa52RcIn-AoGqGOcA")
