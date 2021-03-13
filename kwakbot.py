f2685823cb1018d777f6e393e76a33dbd856f687
Kwak04 <tronixdevelop@gmail.com>
2021-03-14 01:36:21 +0900

    feat: swear detector

--- kwakbot.py
+++ kwakbot.py
@@ -15,7 +15,17 @@ class MyClient(discord.Client):
 
     # Main codes are here
     async def on_message(self, message):
-        print('Message from {0.author}: {0.content}'.format(message))
+        
+        # Swear words detector
+        swear_words = ["씨발", "싸발", "ㅅㅂ", "좆", "ㅈ같네", "병신", "ㅂㅅ", "ㅄ", "쌍", "썅", "ㅆ", "새끼"]
+        for word in swear_words:
+            if word in message.content:
+                try: await message.delete()
+                except: print("<Unable to delete message>")
+                else:
+                    swear_detector_detects = "{0.author}님 욕하지 마세요~~^^"
+                    await message.channel.send(swear_detector_detects.format(message))
+         
         
 client = MyClient()
 client.run(secrets["token"])
