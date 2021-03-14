af3a3fb816d1a5a305b330dda3ac1538ad758551
Tronix Kwak <tronixdevelop@gmail.com>
2021-03-15 01:02:29 +0900

    feat: Made settings file and test (#11)
    * feat: Made settings file and test

--- kwakbot.py
+++ kwakbot.py
@@ -15,7 +15,35 @@ class MyClient(discord.Client):
 
     # Main codes are here
     async def on_message(self, message):
-        print('Message from {0.author}: {0.content}'.format(message))
+        
+        # Swear words detector
+        swear_words = ["씨발", "싸발", "ㅅㅂ", "좆", "ㅈ같네", "병신", "ㅂㅅ", "ㅄ", "쌍", "썅", "ㅆ", "새끼"]
+        for word in swear_words:
+            if word in message.content:
+                try: await message.delete()
+                except: print("<Unable to delete message>")
+                else:
+                    swear_detector_detects = "{0.author}님 욕하지 마세요~~^^"
+                    await message.channel.send(swear_detector_detects.format(message))
+         
+        # Settings test
+        if message.content.startswith(': change'):
+            print('change!!')
+
+            new_string = message.content[9:]  # Text without prefixs
+
+            # Make 'settings' object from 'settings.json'
+            with open('settings.json', 'r', encoding='utf-8') as settings_file:
+                settings = json.load(settings_file)
+
+            # Write data to 'settings' object
+            settings['temp'] = new_string 
+            print(new_string)
+
+            # Write and update settings.json
+            with open('settings.json', 'w', encoding='utf-8') as settings_file:
+                json.dump(settings, settings_file, indent='\t', ensure_ascii=False)
+                
         
 client = MyClient()
 client.run(secrets["token"])
