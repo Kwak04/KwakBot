86e9e7f4e71f7655df0005046be199101f3da1cc
Tronix Kwak <tronixdevelop@gmail.com>
2021-03-15 15:41:01 +0900

    feat: Swear detector (#12)
    * feat: swear detector
    
    * feat: Made an embed
    
    * feat: Show deleted message as a spoiler

--- kwakbot.py
+++ kwakbot.py
@@ -1,6 +1,10 @@
 import discord
 import json
 
+from discord import colour
+from discord import embeds
+from discord import channel
+
 
 # Load token from secrets.json
 with open('secrets.json') as secrets_file:
@@ -16,34 +20,48 @@ class MyClient(discord.Client):
     # Main codes are here
     async def on_message(self, message):
         
-        # Swear words detector
-        swear_words = ["씨발", "싸발", "ㅅㅂ", "좆", "ㅈ같네", "병신", "ㅂㅅ", "ㅄ", "쌍", "썅", "ㅆ", "새끼"]
-        for word in swear_words:
-            if word in message.content:
-                try: await message.delete()
-                except: print("<Unable to delete message>")
-                else:
-                    swear_detector_detects = "{0.author}님 욕하지 마세요~~^^"
-                    await message.channel.send(swear_detector_detects.format(message))
+      message_content = message.content
+      channel = message.channel
+
+      # Don't reply to yourself
+      if message.author == client.user: return
+
+      # Swear words detector
+      swear_words = ["씨발", "싸발", "ㅅㅂ", "시발", "좆", "ㅈ같네", "병신", "ㅂㅅ", "ㅄ", "쌍", "썅", "ㅆ", "새끼"]
+      for word in swear_words:
+          if word in message_content:
+              try: await message.delete()
+              except: print("<Unable to delete message>")
+              else:
+                  swear_detector_message = """<@{0.author.id}>님 욕하지 마세요~~^^
+                  삭제된 메시지: ||{0.content}||"""
+
+                  swear_detector_embed = discord.Embed(
+                          title='욕설이 감지되었습니다.',
+                          description=swear_detector_message.format(message),
+                          colour=0xff0000)
+
+                  await channel.send(embed=swear_detector_embed)
+
          
-        # Settings test
-        if message.content.startswith(': change'):
-            print('change!!')
+      # Settings test
+      if message.content.startswith(': change'):
+          print('change!!')
 
-            new_string = message.content[9:]  # Text without prefixs
+          new_string = message.content[9:]  # Text without prefixs
 
-            # Make 'settings' object from 'settings.json'
-            with open('settings.json', 'r', encoding='utf-8') as settings_file:
-                settings = json.load(settings_file)
+          # Make 'settings' object from 'settings.json'
+          with open('settings.json', 'r', encoding='utf-8') as settings_file:
+              settings = json.load(settings_file)
 
-            # Write data to 'settings' object
-            settings['temp'] = new_string 
-            print(new_string)
+          # Write data to 'settings' object
+          settings['temp'] = new_string 
+          print(new_string)
 
-            # Write and update settings.json
-            with open('settings.json', 'w', encoding='utf-8') as settings_file:
-                json.dump(settings, settings_file, indent='\t', ensure_ascii=False)
-                
+          # Write and update settings.json
+          with open('settings.json', 'w', encoding='utf-8') as settings_file:
+              json.dump(settings, settings_file, indent='\t', ensure_ascii=False)
+              
         
 client = MyClient()
 client.run(secrets["token"])
