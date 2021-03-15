6406188b48bd1a56d4c9288f8dbd2f4fd89d6438
Kwak04 <tronixdevelop@gmail.com>
2021-03-15 16:18:33 +0900

    style: adjust at same indents

--- kwakbot.py
+++ kwakbot.py
@@ -16,32 +16,32 @@ class MyClient(discord.Client):
     # Main codes are here
     async def on_message(self, message):
         
-      message_content = message.content
-      channel = message.channel
+        message_content = message.content
+        channel = message.channel
 
-      # Don't reply to yourself
-      if message.author == client.user: return
+        # Don't reply to yourself
+        if message.author == client.user: return
 
-      # Swear words detector
-      swear_words = ["씨발", "싸발", "ㅅㅂ", "시발", "좆", "ㅈ같네", "병신", "ㅂㅅ", "ㅄ", "쌍", "썅", "ㅆ", "새끼"]
-      for word in swear_words:
-          if word in message_content:
-              try: await message.delete()
-              except: print("<Unable to delete message>")
-              else:
-                  swear_detector_message = """<@{0.author.id}>님 욕하지 마세요~~^^
-                  삭제된 메시지: ||{0.content}||"""
+        # Swear words detector
+        swear_words = ["씨발", "싸발", "ㅅㅂ", "시발", "좆", "ㅈ같네", "병신", "ㅂㅅ", "ㅄ", "쌍", "썅", "ㅆ", "새끼"]
+        for word in swear_words:
+             if word in message_content:
+                 try: await message.delete()
+                 except: print("<Unable to delete message>")
+                 else:
+                     swear_detector_message = """<@{0.author.id}>님 욕하지 마세요~~^^
+                     삭제된 메시지: ||{0.content}||"""
 
-                  swear_detector_embed = discord.Embed(
-                          title='욕설이 감지되었습니다.',
-                          description=swear_detector_message.format(message),
-                          colour=0xff0000)
+                     swear_detector_embed = discord.Embed(
+                             title='욕설이 감지되었습니다.',
+                             description=swear_detector_message.format(message),
+                             colour=0xff0000)
 
-                  await channel.send(embed=swear_detector_embed)
+                     await channel.send(embed=swear_detector_embed)
 
          
-      # Settings test
-      if message.content.startswith(': change'):
+        # Settings test
+        if message.content.startswith(': change'):
           print('change!!')
 
           new_string = message.content[9:]  # Text without prefixs
@@ -58,7 +58,7 @@ class MyClient(discord.Client):
           with open('settings.json', 'w', encoding='utf-8') as settings_file:
               json.dump(settings, settings_file, indent='\t', ensure_ascii=False)
               
-        
+
 client = MyClient()
 client.run(secrets["token"])
 
